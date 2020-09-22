4ec0701f80d030691ac7a3225ce67913f8307f9b
Juriy <jurfed@inbox.ru>
2020-09-22 15:23:12 +0300

    add separate servers startup

--- src/main/java/ru/jurfed/service/ManufacturingRest.java
+++ src/main/java/ru/jurfed/service/ManufacturingRest.java
@@ -1,5 +1,9 @@
 package ru.jurfed.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +23,11 @@ import java.util.concurrent.TimeUnit;
 public class ManufacturingRest {
 
     ScheduledExecutorService service = Executors.newScheduledThreadPool(20);
+    private static final Logger logger = LogManager.getLogger(ManufacturingRest.class);
 
     @RequestMapping(value = "/manufacturing", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public void getOrder(@RequestBody Manufacturing manufacturing) {
-
         service.schedule(new CreatePresents(manufacturing), (int) Math.random() * 2000 + 200, TimeUnit.MILLISECONDS);
-        System.out.println();
-
     }
 
 
@@ -45,11 +47,27 @@ public class ManufacturingRest {
                 URI uri = new URI(baseUrl);
                 HttpEntity<Manufacturing> requestBody = new HttpEntity(manufacturing);
                 restTemplate.postForEntity(uri,requestBody,Manufacturing.class);
+                logger.warn("The goods produced");
             } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
 
         }
+    }
+
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void checkAvailableAfterStartUp() {
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseUrl = "http://localhost:" + 8091 + "/manufacturingStarts";
+        try {
+            URI uri = new URI(baseUrl);
+            HttpEntity<Object> requestBody = new HttpEntity(new Object());
+            restTemplate.getForEntity(uri,Object.class);
+        } catch (Exception e) {
+            logger.warn("Present-system server is not running");
+        }
+
     }
 
 }
